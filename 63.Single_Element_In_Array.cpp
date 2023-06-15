48b01f80f2fc5fbb2653538a634962bd97b21bc7
Abhishek Ranjan <112500327+AbhiRanj123@users.noreply.github.com>
2023-06-15 23:51:02 +0530

    Create 63.Single_Element_In_Array.cpp

---
+++ 63.Single_Element_In_Array.cpp
@@ -0,0 +1,16 @@
+int singleNonDuplicate(vector<int>& arr)
+{
+	// Write your code here
+	int left=0, right=arr.size()-2;
+	
+	while(left<=right){
+		int mid=(left+right)/2;
+		if(arr[mid]==arr[mid ^ 1]){
+			left=mid+1;
+		}
+		else{
+			right=mid-1;
+		}
+	}
+	return arr[left];
+}
